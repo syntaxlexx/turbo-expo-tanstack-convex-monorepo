85786e8b3d38282af50def914bda14605d18a404
SyntaxLexx <spencermainaa@gmail.com>
2025-04-02 14:14:12 +0300

    fix: deploying to prod

--- packages/backend/convex/notes.ts
+++ packages/backend/convex/notes.ts
@@ -1,7 +1,7 @@
 import { mutation, query } from "./_generated/server";
 import { v } from "convex/values";
 import { internal } from "../convex/_generated/api";
-import { Auth } from "convex/server";
+import { type Auth } from "convex/server";
 
 export const getUserId = async (ctx: { auth: Auth }) => {
   return (await ctx.auth.getUserIdentity())?.subject;
